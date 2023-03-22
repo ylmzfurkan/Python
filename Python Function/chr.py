cdfdb299b7cadc7969fc9142a65a790519a6ad43
Furkan Yılmaz <113062519+ylmzfurkan@users.noreply.github.com>
2023-03-22 22:40:39 +0300

    Add files via upload

---
+++ Python Function/chr.py
@@ -0,0 +1,16 @@
+# Python'da chr() fonksiyonu, ASCII değerini alır ve o değerin karşılık geldiği karakteri döndürür.
+# Bu fonksiyon, bir karakterin ASCII değerini bulmak ya da bir ASCII değerine sahip olan karakteri 
+# bulmak için kullanılabilir.
+
+# Örneğin, ASCII değeri 97 olan karakter "a" dır. Aşağıdaki örnek, chr() fonksiyonunu kullanarak 
+# ASCII değeri 97 olan karakteri bulur ve ekrana yazdırır:
+
+# print(chr(97))
+
+# Bu kod, "a" karakterini ekrana yazdıracaktır.
+
+# Benzer şekilde, aşağıdaki örnek, "A" karakterinin ASCII değerini bulur ve ekrana yazdırır:
+
+# print(ord("A"))
+
+# Bu kod, "A" karakterinin ASCII değerini olan 65'i ekrana yazdıracaktır.
